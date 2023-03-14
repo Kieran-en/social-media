19ea7ef4c67d65e49ed88d5f0ca05af358607601
Ryan Djemtchemeu <ryanmambou@gmail.com>
2023-03-14 07:53:16 +0100

    designing chat section

--- frontend/groupamania/src/Components/ChatSection.jsx
+++ frontend/groupamania/src/Components/ChatSection.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Message from './Message'
+import styles from '../Styles/chat.module.css'
 
 function ChatSection() {
   return (
-    <div>
+    <div className={styles.box}>
+      <Message />
+      <Message />
+      <Message />
       <Message />
     </div>
   )
