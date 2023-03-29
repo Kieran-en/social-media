239ef127f3916dc0fdd90ed2e8a0fe9f203d2a08
Ryan Djemtchemeu <ryanmambou@gmail.com>
2023-03-29 06:01:31 +0100

    fix icon on Nav Componenet

--- frontend/groupamania/src/Pages/Nav.js
+++ frontend/groupamania/src/Pages/Nav.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import navImg from '../Images/icon-left-font.png';
+import navImg from '../Images/icon-left-font-monochrome-white.png';
 import navStyle from '../Styles/navbar.module.css';
 
 const Nav = () => {
     return (
         <nav className={navStyle.nav}>
-            <div className="col-3">
+            <div className="col-3 p-3">
             <img src={navImg} className={navStyle.img} alt='logo'/>
             </div>
             <ul className={navStyle.list}>
