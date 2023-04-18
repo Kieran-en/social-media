9633f36b9c05ccb74b32a2ef204c2b9d8fee3159
Ryan Djemtchemeu <ryanmambou@gmail.com>
2023-04-18 09:13:35 +0100

    socket io installed

--- frontend/groupamania/src/Pages/MessagePage.js
+++ frontend/groupamania/src/Pages/MessagePage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from '../Components/NavBar'
 import styles from '../Styles/messagePage.module.css'
 import FriendSection from '../Components/FriendSection'
@@ -7,13 +7,19 @@ import OnlineSection from '../Components/OnlineSection'
 import { useQuery } from 'react-query'
 import { getCurrentUser, getFriends } from '../Services/userService'
 import { useSelector } from 'react-redux'
+import { io } from 'socket.io-client'
 
 function MessagePage() {
 
   const token = useSelector(state => state.token)
+  const [socket, setSocket] = useState()
   const userData = getCurrentUser(token)
   const { username } = userData;
   const {error, status, data: friends} = useQuery('friends', () => getFriends(username))
+
+  useEffect(() => {
+    setSocket(io('http://localhost:5500'))
+  }, [])
 
   return (
     <div>
